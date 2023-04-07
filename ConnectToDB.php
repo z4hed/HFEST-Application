5e15268c4ade4402cbaa33a2937571b4027aea62
Vatsa Shah <vatsashah45@gmail.com>
2023-04-07 13:57:21 -0400

    Displays all the data

--- ConnectToDB.php
+++ ConnectToDB.php
@@ -13,17 +13,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-
-// Execute a SELECT statement
-$sql = "SELECT * FROM employee";
-$result = $conn->query($sql);
-
-// Fetch the results
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo $row['ID'] . " " . $row['first_name'] . " " . $row['last_name'] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
 ?>
